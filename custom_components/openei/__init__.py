158cd9633e718aea38cbfb5e9def542fbebada9c
firstof9 <firstof9@gmail.com>
2021-09-08 10:48:34 -0700

    fix configuration not saving on reconfigure

--- custom_components/openei/__init__.py
+++ custom_components/openei/__init__.py
@@ -147,3 +147,22 @@ async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
+
+
+async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Update listener."""
+
+    _LOGGER.debug("Attempting to reload entities from the %s integration", DOMAIN)
+
+    if config_entry.data == config_entry.options:
+        _LOGGER.debug("No changes detected not reloading entities.")
+        return
+
+    new_data = config_entry.options.copy()
+
+    hass.config_entries.async_update_entry(
+        entry=config_entry,
+        data=new_data,
+    )
+
+    await hass.config_entries.async_reload(config_entry.entry_id)
