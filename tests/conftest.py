672140c4282367838f0cbd41ec82df2e0685187d
firstof9@gmail.com <firstof9@gmail.com>
2024-06-26 16:48:44 -0700

    more test fixing

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,24 @@ import pytest
 pytest_plugins = "pytest_homeassistant_custom_component"
 
 
+# This fixture enables loading custom integrations in all tests.
+# Remove to enable selective use of this fixture
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integration tests."""
     yield
+
+
+# This fixture is used to prevent HomeAssistant from attempting to create and dismiss persistent
+# notifications. These calls would fail without this fixture since the persistent_notification
+# integration is never loaded during a test.
+@pytest.fixture(name="skip_notifications", autouse=True)
+def skip_notifications_fixture():
+    """Skip notification calls."""
+    with patch("homeassistant.components.persistent_notification.async_create"), patch(
+        "homeassistant.components.persistent_notification.async_dismiss"
+    ):
+        yield
 
 
 @pytest.fixture(name="mock_api")
