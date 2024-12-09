e0736f07a58cf0f690fd7bb0edb69864699dc705
firstof9@gmail.com <firstof9@gmail.com>
2024-12-08 19:26:03 -0700

    feat: add fixed charges sensor

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -1,5 +1,6 @@
 """Tests for sensors."""
 
+import logging
 import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
@@ -7,6 +8,7 @@ from custom_components.openei.const import DOMAIN
 from tests.const import CONFIG_DATA
 
 FAKE_MINCHARGE_SENSOR = "sensor.fake_utility_co_minimum_charge"
+FAKE_FIXEDCHARGE_SENSOR = "sensor.fake_utility_co_fixed_charge_first_meter"
 FAKE_CURRENT_RATE_SENSOR = "sensor.fake_utility_co_current_energy_rate"
 FAKE_CURRENT_RATE_STRUCTURE_SENSOR = (
     "sensor.fake_utility_co_current_energy_rate_structure"
@@ -15,25 +17,29 @@ FAKE_CURRENT_RATE_STRUCTURE_SENSOR = (
 pytestmark = pytest.mark.asyncio
 
 
-async def test_sensors(hass, mock_api):
+async def test_sensors(hass, mock_api, caplog):
     """Test settting up entities."""
     entry = MockConfigEntry(
         domain=DOMAIN,
         title="Fake Utility Co",
         data=CONFIG_DATA,
     )
+    with caplog.at_level(logging.DEBUG):
+        entry.add_to_hass(hass)
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
 
-    entry.add_to_hass(hass)
-    assert await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
+        state = hass.states.get(FAKE_MINCHARGE_SENSOR)
+        assert state is not None
+        assert state.state == "unknown"
 
-    state = hass.states.get(FAKE_MINCHARGE_SENSOR)
-    assert state is not None
-    assert state.state == "unknown"
+        state = hass.states.get(FAKE_CURRENT_RATE_SENSOR)
+        assert state is not None
+        assert state.attributes["all_rates"] == [0.24477, 0.06118, 0.19847, 0.06116]
 
-    state = hass.states.get(FAKE_CURRENT_RATE_SENSOR)
-    assert state is not None
-    assert state.attributes["all_rates"] == [0.24477, 0.06118, 0.19847, 0.06116]
+        state = hass.states.get(FAKE_CURRENT_RATE_STRUCTURE_SENSOR)
+        assert state is not None
 
-    state = hass.states.get(FAKE_CURRENT_RATE_STRUCTURE_SENSOR)
-    assert state is not None
+        state = hass.states.get(FAKE_FIXEDCHARGE_SENSOR)
+        assert state is not None
+        assert state.state == "16.91"
